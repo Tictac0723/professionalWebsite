21ebd57e7a7079f45f2f5da1823baf434b10d0f3
Tictac0723 <taylorclark0723@gmail.com>
2017-08-04 14:49:13 -0500

    looking pretty good

--- slab-main-files/html/image-bg/php/sendmail.php
+++ slab-main-files/html/image-bg/php/sendmail.php
@@ -15,7 +15,7 @@ if( isset($name) && isset($email) ) {
 	}
 
 	// Email will be send
-	$to = "mailexample62@gmail.com"; // Change with your email address
+	$to = "taylorclark0723@gmail.com"; // Change with your email address
 	$sub = "$subject from Cv"; // You can define email subject
 	// HTML Elements for Email Body
 	$body = <<<EOD
